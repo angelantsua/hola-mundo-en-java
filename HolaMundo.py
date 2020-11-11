b669124a79bb7e6761c458358f5c3fb02534b4dd
angelantsua <angelopower23@gmail.com>
2020-11-11 11:33:51 +0100

    Saludo en colores

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,5 +1,5 @@
 # Muestra por pantalla la frase "Hola mundo!"
 naranja = "\033[1;33m";
 azul = "\033[4;34m";
-print(naranja + "Hola mundo!")
-print(azul + "Aquí estamos haciendo pruebas con Git.")
+print("Hola mundo!")
+print("Aquí estamos haciendo pruebas con Git.")
